8708ab27821cf24e87a2f53a3865a2f162e58879
josejacin <joseynene@hotmail.com>
2018-03-23 19:10:18 +0100

    Seccion5.74.pipe.Date.Fecha

--- Sección 5.Pipes.Transforman los valores mostrados en pantalla/Angular/3.Pipes/src/app/app.component.ts
+++ Sección 5.Pipes.Transforman los valores mostrados en pantalla/Angular/3.Pipes/src/app/app.component.ts
@@ -10,8 +10,8 @@ export class AppComponent {
   array = [1,2,3,4,5,6,7,8,9,10];
   pi = Math.PI;
   a = 0.234;
-  salario=1234.5;
-  heroe={
+  salario = 1234.5;
+  heroe = {
     nombre:"Logan",
     clave:"Wolverine",
     edad:500,
@@ -20,7 +20,8 @@ export class AppComponent {
       casa: "19"
     }
   }
-  valorDePromesa=new Promise((resolve,reject) => {
+  valorDePromesa = new Promise((resolve,reject) => {
     setTimeout(() => resolve('Llegaron los datos'),3500);
-  })
+  });
+  fecha = new Date();
 }
