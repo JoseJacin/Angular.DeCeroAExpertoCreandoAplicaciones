de86b325d737dba9c143e604be5cdf181ffff919
josejacin <joseynene@hotmail.com>
2018-03-24 14:13:10 +0100

    Seccion5.79.resolucionDeLaTareaPractica3-Pipes

--- Sección 5.Pipes.Transforman los valores mostrados en pantalla/Angular/3.Pipes/src/app/pipes/contrasena.pipe.ts
+++ Sección 5.Pipes.Transforman los valores mostrados en pantalla/Angular/3.Pipes/src/app/pipes/contrasena.pipe.ts
@@ -4,20 +4,52 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'contrasena'
 })
 export class ContrasenaPipe implements PipeTransform {
-  transform(value: any, activado : boolean = true): string {
+  transform(value: any, activar : boolean = true): string {
 
-		let contrasena:string = "";
-
-		if (activado) {
+		if (activar) {
+			let contrasena:string = "";
 			for(let i in value) {
-				contrasena = contrasena + '*';
+				contrasena += '*';
 			}
+			return contrasena;
 		} else {
-			contrasena = value;
+			return value;
 		}
 
+		/* Otras formas de hacerlo */
+		/* Forma 1 */
+		/*
+		let re = /./gi;
+    if(activar)  value = value.replace(re,"*")
+    return value;
+		*/
 
-		return contrasena;
+		/* Forma 2 */
+		/*
+		if (ocultar) {
+			return new Array(value.length+1).join("*");
+		} else {
+			return value;
+		}
+		*/
+
+		/* Forma 3 */
+		/*
+		if (ocultar) {
+			return '*'.repeat(value.length);
+		} else {
+			return value;
+		}
+		*/
+
+		/* Forma 4 */
+		/*
+		return activar ?Array(value.length+1).join("*"):value;
+		*/
+
+		/* Forma 5 */
+		/*
+		return activar ? value.replace(/[^\w]|\w/gi, "*"):value;
+		*/
   }
-
 }
