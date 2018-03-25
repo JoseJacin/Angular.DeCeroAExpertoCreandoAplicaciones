3e57c5c9eb334ec6ab54e94b30e022e024c9921b
josejacin <joseynene@hotmail.com>
2018-03-25 13:42:41 +0200

    Seccion6.87.httpClient-Service.ConectandonosASpotify

--- Sección 6.Aplicacion 3.SpotiApp/Angular/4.SpotiApp/src/app/app.module.ts
+++ Sección 6.Aplicacion 3.SpotiApp/Angular/4.SpotiApp/src/app/app.module.ts
@@ -3,11 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { app_routing } from './app.routes';
 
+import { HttpClientModule } from '@angular/common/http';
+
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
+//Services
+import { SpotifyService } from './services/spotify.service';
 
 @NgModule({
   declarations: [
@@ -18,9 +23,12 @@ import { NavbarComponent } from './components/shared/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-		app_routing
+		app_routing,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    SpotifyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
