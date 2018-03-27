641d13d19a897b998fc537f9986cd225f5951a46
josejacin <joseynene@hotmail.com>
2018-03-28 01:24:14 +0200

    Seccion6.88.enviandoElTokenEnLosHeadersDeLaPeticion

--- Sección 6.Aplicacion 3.SpotiApp/Angular/4.SpotiApp/src/app/services/spotify.service.ts
+++ Sección 6.Aplicacion 3.SpotiApp/Angular/4.SpotiApp/src/app/services/spotify.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class SpotifyService {
+
+	artistas:any[] = [];
 
   constructor(public http:HttpClient) {
 		console.log("Servicio de Spotify listo");
@@ -11,9 +13,10 @@ export class SpotifyService {
 	getArtistas() {
 		let url = 'https://api.spotify.com/v1/search?query=metallica&type=artist&limit=20';
 
-		this.http.get(url).subscribe(resp => {
-			console.log(resp);
-		})
-	}
+		let headers = new HttpHeaders({
+			'authorization': 'Bearer BQDr-VMSVnuP6T3nvbnI_-ns8bV-asZ0O3_33vohY1H83WiRShcf3h807iIwvWa2h0AZxlI1_ohCpVrp-Rw'
+		});
 
+		return this.http.get(url, {headers});
+	}
 }
