ae6582f4b9b75457c5760d96f6f650d3ddd5aa5c
josejacin <joseynene@hotmail.com>
2018-04-13 22:42:13 +0200

    Seccion7.113.ionic.AgregandoAlServicio-AlertasYRetornandoALaPantallaPrincipal

--- Seccion 7.Ionic/Ionic/5.Deseos-ionic/src/pages/agregar/agregar.component.ts
+++ Seccion 7.Ionic/Ionic/5.Deseos-ionic/src/pages/agregar/agregar.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController, NavController } from 'ionic-angular';
 
 //Clases
 import { Lista, ListaItem } from '../../app/clases';
 
+//Servicios
+import { ListaDeseosService } from '../../app/services/lista-deseos.service';
 
 @Component({
   selector: 'app-agregar',
@@ -11,12 +13,16 @@ import { Lista, ListaItem } from '../../app/clases';
 })
 export class AgregarComponent implements OnInit {
 
-  nombreLista:string;
+  nombreLista:string = "";
   nombreItem:string  = "";
 
   items:ListaItem[] = [];
 
-  constructor() { }
+  constructor(
+    public alertCtrl:AlertController,
+    public navCtrl:NavController,
+    public _listaDeseos:ListaDeseosService
+  ) { }
 
   ngOnInit() { }
 
@@ -37,4 +43,22 @@ export class AgregarComponent implements OnInit {
     this.items.splice(index,1);
   }
 
+  guardarLista() {
+    if(this.nombreLista.length == 0) {
+      let alert = this.alertCtrl.create({
+        title: 'Nombre de la lista',
+        subTitle: 'El nombre de la lista es necesario',
+        buttons: ['OK']
+      });
+      alert.present();
+      return;
+    }
+
+    let lista = new Lista(this.nombreLista);
+    lista.items = this.items;
+
+    this._listaDeseos.listas.push(lista);
+
+    this.navCtrl.pop();
+  }
 }
