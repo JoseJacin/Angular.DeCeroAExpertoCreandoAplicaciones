5426ef8e4101d02aef90306b660b765d774a8d2a
josejacin <joseynene@hotmail.com>
2018-04-21 13:02:24 +0200

    Seccion7.118.ionic.PipesImpuros

--- Seccion 7.Ionic/Ionic/5.Deseos-ionic/src/app/app.module.ts
+++ Seccion 7.Ionic/Ionic/5.Deseos-ionic/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ListaDeseosService } from './services/lista-deseos.service';
 
 //Pipe
 import { PlaceHolderPipe } from './pipes/placeholder.pipe';
+import { PendientesPipe } from './pipes/pendientes.pipe';
 
 //Otros
 import { StatusBar } from '@ionic-native/status-bar';
@@ -24,6 +25,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     PlaceHolderPipe,
+    PendientesPipe,
     PendientesComponent,
     TerminadosComponent,
 		AgregarComponent,
