f49e7066502d565b9fdbeb942f6247dac9c23dd5
Jose <joseynene@hotmail.com>
2018-07-24 23:53:16 +0200

    Seccion6.90.introduccionALasPeticionesHTTP

--- Seccion 6.Apliccion 3.SpotiApp/Angular/4.SpotiApp/src/app/components/home/home.component.ts
+++ Seccion 6.Apliccion 3.SpotiApp/Angular/4.SpotiApp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HomeComponent implements OnInit {
+
+  /*
+   Esto es solo para explicar las peticiones HTTP
+  //paises: any[] = [];
+
+  constructor(private http: HttpClient) {
+    console.log('Constructor del home hecho');
+    // Se configura una petición en la que en algún momento "alguien" se va a suscribir
+    this.http
+        .get('https://restcountries.eu/rest/v2/lang/es')
+        .subscribe((respuesta: any) => {
+          this.paises = respuesta;
+          console.log(respuesta);
+        });
+  }
+  */
 
   constructor() { }
 
